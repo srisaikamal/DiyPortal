393fe17c5693547f0b2cd75a53ce194f4118b5ad
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2020-04-12 19:05:49 +0530

    intial version1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby multiple language starter`,
-    description: `A simple but feature rich starter boilerplate for creating a multi-language Gatsby website (Internationalization / i18n) without third party plugins or packages and also focused on SEO. This starter also contains other main Gatsby configurations you might need.`,
-    author: `@diogorodrigues`,
-    siteUrl: `https://iceberg-gatsby-multilang.netlify.com/`,
+    title: `DiyProtal`,
+    description: `Simple Day to day life hacks `,
+    author: `@vsskamal`,
+    siteUrl: `https://wizardly-hypatia-367de7.netlify.com/`,
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
