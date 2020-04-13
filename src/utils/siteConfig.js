2884f5c92b9a0ca106d40e3e99a75109892bad6b
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2020-04-13 13:25:31 +0530

    Page title

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://www.diyportal.me/`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Diy Portal`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Get latest Thoughts, Stories, and Ideas on Diy's, life hacks through DiyPortal. DiyPortal provides all the latest simple life hacks, growth hacking tips...`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
